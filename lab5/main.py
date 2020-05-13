14d3f146d4f3c5099264dcb161cd924c00e2550d
Azbesciak <witkups@gmail.com>
2020-05-13 22:04:15 +0200

    Add PCA and fix charts preparation - lab 5

--- lab5/main.py
+++ lab5/main.py
@@ -1,4 +1,6 @@
 import math
+
+from sklearn.decomposition import PCA
 from sklearn.manifold import MDS, TSNE, SpectralEmbedding, Isomap
 from sklearn.datasets import make_swiss_roll
 import pandas as pd
@@ -23,17 +25,18 @@ def get_nations_data() -> pd.DataFrame:
 def visualize_embedding(embeddings, data, title, add_labels):
     rows = int(len(embeddings) ** 0.5)
     columns = math.ceil(len(embeddings) / rows)
-    fix, axs = plt.subplots(rows, columns, figsize=(12, 8))
-    fix.suptitle(title)
+    fig, axs = plt.subplots(rows, columns, figsize=(12, 8))
     for i, (name, embedding) in enumerate(embeddings):
-        row, col = i // rows, i % columns
+        row, col = i // columns, i % columns
         x_transformed = embedding.fit_transform(data)
         colors = cm.rainbow(np.linspace(0, 1, len(x_transformed)))
         axs[row, col].scatter(x_transformed[:, 0], x_transformed[:, 1], color=colors)
         axs[row, col].set_title(name)
         if add_labels:
             for row_data, (x, y) in zip(data.T, x_transformed):
-                axs[row, col].text(x, y, f"{row_data}", )
+                axs[row, col].text(x, y, f"{row_data}")
+
+    fig.suptitle(title, fontsize=12, y=1)
     plt.show()
 
 
@@ -44,11 +47,13 @@ if __name__ == '__main__':
         ("Nations", get_nations_data(), True),
     ]
     for name, data_set, add_labels in data_sets:
-        iso = Isomap(n_components=2, n_neighbors=5)
+        iso5 = Isomap(n_components=2, n_neighbors=5)
+        iso3 = Isomap(n_components=2, n_neighbors=3)
         mds = MDS(n_components=2)
         tsne = TSNE(n_components=2)
         se = SpectralEmbedding(n_components=2)
+        pca = PCA(n_components=2)
         visualize_embedding(
-            [("iso", iso), ("mds", mds), ("t-sne", tsne), ("se", se)],
+            [("Isomap (n = 5)", iso5), ("Isomap (n = 3)", iso3), ("PCA", pca), ("MDS", mds), ("T-sne", tsne), ("SE", se)],
             data_set, name, add_labels
         )
