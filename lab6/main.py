299f21adf4938a2eede35e5c725343120aef363d
Azbesciak <witkups@gmail.com>
2020-05-18 23:02:36 +0200

    Add sketch for lab 6 with scikit svd compression

---
+++ lab6/main.py
@@ -0,0 +1,60 @@
+import argparse
+import matplotlib.image as mpig
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.linalg import svd
+
+
+def get_input_params():
+    parser = argparse.ArgumentParser()
+    parser.add_help = True
+    parser.add_argument("-f", "--file", help="path to the image", required=True)
+    parser.add_argument("-out", help="output image path")
+    parser.add_argument("-svd", help="SVD implementation to use", choices=["custom", "scikit"], default="custom")
+    parser.add_argument("-k", help="number of singular values used for compression", type=int)
+    return parser.parse_args()
+
+
+def make_result_presentation(result, output_path):
+    plt.axes([0, 0, 1, 1])
+    plt.axis("off")
+    plt.imshow(result)
+    if output_path is None:
+        plt.show()
+    else:
+        plt.savefig(params.out, bbox_inches='tight', pad_inches=0)
+
+
+def read_input_image(file_path: str):
+    image = mpig.imread(file_path)
+    if image.dtype == np.uint8:
+        image = image / 255
+    return image
+
+
+def scikit_svg(image: np.array, k: int) -> np.array:
+    U, s, V = svd(image, full_matrices=False)
+    return np.dot(U[:, :k], np.dot(np.diag(s[:k]), V[:k, :]))
+
+
+def custom_svg(image: np.array, k: int) -> np.array:
+    return image
+
+
+def compress(image: np.array, method: str, k: int or None):
+    original_shape = image.shape
+    if len(original_shape) == 3:
+        image = image.reshape((original_shape[0], original_shape[1] * 3))
+    reconst_matrix = scikit_svg(image, k) if method == "scikit" else custom_svg(image, k)
+    if len(original_shape) == 3:
+        reconst_matrix = reconst_matrix.reshape(original_shape)
+    return reconst_matrix
+
+
+if __name__ == '__main__':
+    params = get_input_params()
+    if params.k is not None and params.k < 0:
+        raise ValueError("k must be not negative or not defined")
+    image = read_input_image(params.file)
+    compressed_image = compress(image, params.svd, params.k)
+    make_result_presentation(compressed_image, params.out)
--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@
 - lab 3 - [content](https://www.cs.put.poznan.pl/ibladek/students/skaiwd/lab3/zadania_eigen.pdf) and [solution](lab3/main.py)
 - lab 4 - [content](https://www.cs.put.poznan.pl/ibladek/students/skaiwd/lab4/zadania_pca.pdf) and [solution](lab4/main.py)
 - lab 5 - [content](https://www.cs.put.poznan.pl/ibladek/students/skaiwd/lab5/zadania_mds.pdf), [report[PL]](lab5/README.md), and [code](lab5/main.py)
+- lab 6 - [content](https://www.cs.put.poznan.pl/ibladek/students/skaiwd/lab6/homework_svd.pdf) and [solution](lab6/main.py)
